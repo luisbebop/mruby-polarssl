02221d7b5ef06917fb2b550abce083685fab7f09
luisbebop <luisbebop@gmail.com>
2013-11-09 01:17:46 -0200

    added PolarSSL::Entropy

--- src/polarssl.c
+++ src/polarssl.c
@@ -1,19 +1,63 @@
 #include "mruby.h"
-#include <stdio.h>
-#include <string.h>
-#include "mruby/array.h"
-#include "mruby/class.h"
 #include "mruby/data.h"
-#include "mruby/string.h"
-#include "mruby/variable.h"
+#include "polarssl/entropy.h"
 
-#include <err.h>
+static void lib_entropy_free(mrb_state *mrb, void *ptr) {
+  entropy_context *entropy = ptr;
 
-void mrb_mruby_polarssl_gem_init(mrb_state *mrb) {
-	struct RClass *d;
-	d = mrb_define_module(mrb, "PolarSSL");
+  if (entropy != NULL) {
+    mrb_free(mrb, entropy);
+  }
 }
 
-void mrb_mruby_polarssl_gem_final(mrb_state *mrb) {
+static struct mrb_data_type mrb_entropy_type = { "ENTROPY", lib_entropy_free };
+
+static void entropycheck(mrb_state *mrb, mrb_value self, entropy_context **entropyp) {
+  entropy_context *entropy;
+
+  entropy = (entropy_context *)DATA_PTR(self);
+  if (!entropy) {
+    mrb_raise(mrb, E_RUNTIME_ERROR, "no entropy found (BUG?)");
+  }
+  if (entropyp) *entropyp = entropy;
+}
+
+static mrb_value mrb_entropy_gather(mrb_state *mrb, mrb_value self) {
+  entropy_context *entropy;
+
+  entropycheck(mrb, self, &entropy);
+
+  if( entropy_gather( entropy ) == 0 ) {
+	return mrb_true_value();
+  } else {
+	return mrb_false_value();
+  }
+}
+
+static mrb_value mrb_entropy_init(mrb_state *mrb, mrb_value self) {
+  entropy_context *entropy;
+
+  entropy = (entropy_context *)DATA_PTR(self);
+  if (entropy) {
+    lib_entropy_free(mrb, entropy);
+  }
+  DATA_TYPE(self) = &mrb_entropy_type;
+  DATA_PTR(self) = NULL;
+
+  entropy = (entropy_context *)mrb_malloc(mrb, sizeof(*entropy));
+  DATA_PTR(self) = entropy;
+  entropy_init(entropy);
+  return self;
+}
+
+void mrb_mruby_polarssl_gem_init(mrb_state *mrb) {
+	struct RClass *p, *e;
 	
+	p = mrb_define_module(mrb, "PolarSSL");
+	e = mrb_define_class_under(mrb, p, "Entropy", mrb->object_class);
+	mrb_define_method(mrb, e, "initialize", mrb_entropy_init, MRB_ARGS_NONE());
+	mrb_define_method(mrb, e, "gather", mrb_entropy_gather, MRB_ARGS_NONE());
+}
+
+void mrb_mruby_polarssl_gem_final(mrb_state *mrb) {	
 }
--- test/polarssl.rb
+++ test/polarssl.rb
@@ -4,4 +4,17 @@ if Object.const_defined?(:PolarSSL)
   assert("PolarSSL") do
     PolarSSL.class == Module
   end
+  
+  assert('PolarSSL::Entropy') do
+    PolarSSL::Entropy.class == Class
+  end
+  
+  assert('PolarSSL::Entropy#new') do
+    entropy = PolarSSL::Entropy.new
+  end
+  
+  assert('PolarSSL::Entropy#gather') do
+    entropy = PolarSSL::Entropy.new
+    entropy.gather() == true
+  end
 end
