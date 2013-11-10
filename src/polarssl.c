f89a2c55507c5dcec0e166fc6a41831044427cb4
luisbebop <luisbebop@gmail.com>
2013-11-10 16:27:41 -0200

    added PolarSSL::SSL#close

--- src/polarssl.c
+++ src/polarssl.c
@@ -252,6 +252,14 @@ static mrb_value mrb_ssl_close_notify(mrb_state *mrb, mrb_value self) {
 	return mrb_true_value();
 }
 
+static mrb_value mrb_ssl_close(mrb_state *mrb, mrb_value self) {
+	ssl_context *ssl;
+	
+	ssl = DATA_CHECK_GET_PTR(mrb, self, &mrb_ssl_type, ssl_context);
+	memset(ssl, 0, sizeof(ssl_context));
+	return mrb_true_value();
+}
+
 void mrb_mruby_polarssl_gem_init(mrb_state *mrb) {
 	struct RClass *p, *e, *c, *s;
 	
@@ -286,6 +294,7 @@ void mrb_mruby_polarssl_gem_init(mrb_state *mrb) {
 	mrb_define_method(mrb, s, "write", mrb_ssl_write, MRB_ARGS_REQ(1));
 	mrb_define_method(mrb, s, "read", mrb_ssl_read, MRB_ARGS_REQ(1));
 	mrb_define_method(mrb, s, "close_notify", mrb_ssl_close_notify, MRB_ARGS_NONE());
+	mrb_define_method(mrb, s, "close", mrb_ssl_close, MRB_ARGS_NONE());
 }
 
 void mrb_mruby_polarssl_gem_final(mrb_state *mrb) {	
--- test/polarssl.rb
+++ test/polarssl.rb
@@ -197,5 +197,22 @@ if Object.const_defined?(:PolarSSL)
     p ssl.read(4)
     ssl.close_notify
   end
+  
+  assert('PolarSSL::SSL#close') do 
+    socket = TCPSocket.new('polarssl.org', 443)
+    entropy = PolarSSL::Entropy.new
+    ctr_drbg = PolarSSL::CtrDrbg.new(entropy)
+    ssl = PolarSSL::SSL.new
+    ssl.set_endpoint(PolarSSL::SSL::SSL_IS_CLIENT)
+    ssl.set_authmode(PolarSSL::SSL::SSL_VERIFY_NONE)
+    ssl.set_rng(ctr_drbg)
+    ssl.set_socket(socket)
+    ssl.handshake
+    ssl.write("GET / HTTP/1.0\r\nHost: polarssl.org\r\n\r\n")
+    p ssl.read(4)
+    ssl.close_notify
+    socket.close
+    ssl.close
+  end
       
 end
